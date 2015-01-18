91116a33d47712483dfcc96a685e154f6bdcf801
regisw areja <regiswareja@gmail.com>
2015-01-19 03:45:54 +0900

    config

--- controllers/like_controller.php
+++ controllers/like_controller.php
@@ -1,18 +1,61 @@
 <?php
 /**
 * Controller script responsible for handling likes.
- @regis **/
-$username="suny";
-$password="suny";
-$hostname="54.149.232.134"
+* @author regis **/
 
-//connection to the database
+$action = $_POST['action'];
 
-$dbhandle=new mysqli($hostname,$username,$password,"idea_db");
+if ($action == 'create_like')
+{
+  function create_like($idea_id,$user_liked)
+  {
+  // global variables from config/db_config.php
+  global $ideas_db_table;
+  global $db_hostname;
+  global $db_user;
+  global $db_password;
+  global $idea_db;
 
-//$query="SELECT * FROM likes"
+  //date format
+  $date = date("Y-m-d H:i:s");
+  $query = "INSERT INTO likes(id,idea_id,user_liked,date_created) values('NULL','$idea_id','$user_liked','$date')";
+  return send_query($query, $db_hostname, $db_user, $db_password, $db_use);
+  }
+  header("Location:like.php");
+  die();
+}
+else
+{
+  echo "<br> CHECK YOUR CONNECTION";
+  header("Location: ../like.php?error=404_not_found");
+  die();
+}
 
-$result=$dbhandle->query("SELECT * FROM likes");
-mysqli_query($dbhandle,"INSERT INTO likes(id,idea_id,user_liked,date_created) values('NULL','$'
+  /**
+* This function is responsible for removing likes
+* @author Regis
+*/
+if ($action == 'remove_like')
+{
+  function remove_like($like_id)
+  {
+  // global variables from config/db_config.php
+  global $ideas_db_table;
+  global $db_hostname;
+  global $db_user;
+  global $db_password;
+  global $idea_db;
+  $query = "DELETE FROM likes WHERE likes . id='$like_id'";
+  return send_query($query, $db_hostname, $db_user, $db_password, $db_use);
+  }
+  header("Location:removelike.php")
+  die();
+}
+else
+{
+  echo "<br> CHECK YOUR CONNECTION";
+  header("Location: ../removelike.php?error=404_not_found");
+  die();
+}
 
 ?>
