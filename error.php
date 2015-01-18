b7c7724ee016fc48537dfaa946fc13f3089fcae8
Delvison Castillo <delvisoncastillo@gmail.com>
2015-01-19 02:16:45 +0900

    added includes for navbar and footer

--- error.php
+++ error.php
@@ -1,7 +1,15 @@
 <html>
   <body>
+    <?php
+    // load navbar
+    require $_SERVER['DOCUMENT_ROOT'].'/idea/includes/navbar.php';
+    ?>
     <h1>
       Oops! We are experiencing technical difficulties.
     </h1>
+    <?php
+    // load navbar
+    require $_SERVER['DOCUMENT_ROOT'].'/idea/includes/footer.php';
+    ?>
   </body>
 </html>
