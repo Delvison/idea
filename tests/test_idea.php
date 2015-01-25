5f91c870914ca906af44811c96da17a9a5eceb2b
Delvison Castillo <delvisoncastillo@gmail.com>
2015-01-25 21:18:46 +0900

    include for scripts made

--- tests/test_idea.php
+++ tests/test_idea.php
@@ -5,13 +5,10 @@ check_login();
 
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="/idea/css/bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="/idea/css/style.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-  <script src="/idea/js/jquery-2.1.3.min.js"></script>
+  <?php
+  // load scripts
+  require $_SERVER['DOCUMENT_ROOT'].'/idea/includes/scripts.php';
+  ?>
 </head>
 
 <body>
